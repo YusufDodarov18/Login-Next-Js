ac0cb58b358e1e0115dcb97e7801d253033a52fb
YusufDodarov18 <email@github.com>
2026-01-01 13:03:05 +0500

    first commit

---
+++ src/app/others/location.ts
@@ -0,0 +1,29 @@
+export const tjLocalization = {
+  // signUp: {
+  //   continueButton: 'Давом додан',   
+  //   start: {
+  //     title: 'Ҳисоб кушоед',
+  //     subtitle: 'Хуш омадед! Барои оғоз маълумотҳоро пур кунед.',
+  //   },
+  //   // Агар хоҳед, дигар матнҳо ҳам тағйир дода мешавад
+  // },
+  // signIn: {
+  //   continueButton: 'Давом додан',
+  //   start: {
+  //     title: 'Ворид шудан',
+  //     subtitle: 'Хуш омадед! Барои ворид шудан маълумотҳоро пур кунед.',
+  //   },
+  // },
+  formButtonPrimary: 'Давом додан',     
+  // socialButtonsBlockButton: 'Бо {{provider}} ворид шудан',
+  // dividerText: 'ё',
+  // formFieldLabel__firstName: 'Ном',
+  // formFieldLabel__lastName: 'Насаб',
+  // formFieldLabel__username: 'Номи корбар',
+  // formFieldLabel__emailAddress: 'Суроғаи электронӣ',
+  // formFieldLabel__password: 'Парол',
+  // formFieldInputPlaceholder__firstName: 'Ном',
+  // formFieldInputPlaceholder__lastName: 'Насаб',
+  // formFieldInputPlaceholder__emailAddress: 'Суроғаи электрониатонро ворид кунед',
+  // formFieldInputPlaceholder__password: 'Паролро ворид кунед',
+}
